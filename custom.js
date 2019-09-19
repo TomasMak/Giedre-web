59550e25821ee1bcbca20c5a95188d45a698741c
TomasMak <t.makauskas@gmail.com>
2019-09-19 21:32:06 +0200

    Fixed hover effects and visibility for next and prev areas

--- custom.js
+++ custom.js
@@ -15,27 +15,13 @@ showDivs("project4", projectIndexes.project4);
 // showDivs("project6", projectIndexes.project6);
 // showDivs("project7", projectIndexes.project7);
 
-// Image library 
-const customCursor = document.getElementById('mousemove')
-const haderName = document.getElementById('header')
-customCursor.innerText = 'Deimante'
-
-haderName.onmouseover = 
- hideMouseMove = () => {
-	customCursor.style.visibility = 'hidden'
-}
-haderName.onmouseout = 
-showMouseMove = () => {
-    customCursor.style.visibility = 'visible'
-}
-
 function plusDivs(project, n) {
 	showDivs(project, projectIndexes[project] += n);
 }
 
 function showDivs(project, index) {
 	let i;
-    let x = document.getElementById(project).getElementsByClassName("slidess");
+	let x = document.getElementById(project).getElementsByClassName("slidess");
 	if (index > x.length) { index = 1 }
 	if (index < 1) { index = x.length }
 	for (i = 0; i < x.length; i++) {
@@ -43,28 +29,28 @@ function showDivs(project, index) {
 	}
 	x[index - 1].style.display = "block";
 	// document.getElementById(project).getElementsByClassName("pagination")[0].innerText = index + ' / ' + x.length;
-    // let text = document.getElementById(project).getElementsByClassName("pagination")[0].innerText = index + ' / ' + x.length;
-    projectIndexes[project] = index;
-    let elements = document.getElementById(project).querySelector('.imgslide').children;
+	// let text = document.getElementById(project).getElementsByClassName("pagination")[0].innerText = index + ' / ' + x.length;
+	projectIndexes[project] = index;
+	let elements = document.getElementById(project).querySelector('.imgslide').children;
 	let imgNames = [];
 	for (let i = 0; i < elements.length; i++) {
 		// if (elements[i].src !== undefined) {
-			imgNames.push(elements[i].children[0].children[0].alt);
-			// document.getElementById(project).getElementsByClassName("img-name")[0].innerText = imgNames[index - 1];
+		imgNames.push(elements[i].children[0].children[0].alt);
+		// document.getElementById(project).getElementsByClassName("img-name")[0].innerText = imgNames[index - 1];
 		// }
-    }
-    // console.log( imgNames[0])
-    document.getElementById('mousemove').innerText = imgNames[index - 1]
+	}
+	// console.log( imgNames[0])
+	document.getElementById('mousemove').innerText = imgNames[index - 1]
 	document.getElementById(project).onmouseover = function () { mouseOver() }
 	document.getElementById(project).onclick = function () { mouseOver() }
 	document.getElementById(project).onmouseout = function () { mouseOut() };
 	let prjct = document.getElementById(project)
-	function mouseOver() {	
+	function mouseOver() {
 	}
 
 	function mouseOut() {
 	}
-	
+
 }
 
 
@@ -94,11 +80,44 @@ function openNav() {
 	document.getElementById('mousemove').style.display = 'none'
 	document.getElementById("body").style.cursor = 'default'
 
-  }
-  
-  function closeNav() {
+}
+
+function closeNav() {
 	document.getElementById("myNav").style.display = "none";
 	document.getElementById('mousemove').style.display = 'block'
 	document.getElementById("body").style.cursor = 'none'
 
-  }
+}
+
+// Image library 
+const customCursor = document.getElementById('mousemove')
+const haderName = document.getElementById('header')
+const arrowNext = document.getElementById('nextArrow')
+const arrowPrev = document.getElementById('prevArrow')
+customCursor.innerText = 'Deimante'
+
+haderName.onmouseover =
+	hideMouseMove = () => {
+		customCursor.style.visibility = 'hidden'
+	}
+haderName.onmouseout =
+	showMouseMove = () => {
+		customCursor.style.visibility = 'visible'
+	}
+arrowNext.onmouseover =
+	hideMouseMove = () => {
+		customCursor.style.visibility = 'hidden'
+	}
+arrowNext.onmouseout =
+	showMouseMove = () => {
+		customCursor.style.visibility = 'visible'
+	}
+arrowPrev.onmouseover =
+	hideMouseMove = () => {
+		customCursor.style.visibility = 'hidden'
+	}
+arrowPrev.onmouseout =
+	showMouseMove = () => {
+		customCursor.style.visibility = 'visible'
+	}
+
