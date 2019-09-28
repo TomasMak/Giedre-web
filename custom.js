fd934db5e9d759eb880fb4ec48d3787be0b3f265
TomasMak <t.makauskas@gmail.com>
2019-09-28 21:31:03 +0200

    Added resized Roos-Anna project images

--- custom.js
+++ custom.js
@@ -5,7 +5,7 @@ let projectIndexes = {
 	project4: 1,
 	project5: 1,
 	project6: 1,
-	// project7: 1,
+	project7: 1,
 }
 showDivs("project1", projectIndexes.project1);
 showDivs("project2", projectIndexes.project2);
@@ -13,7 +13,7 @@ showDivs("project3", projectIndexes.project3);
 showDivs("project4", projectIndexes.project4);
 showDivs("project5", projectIndexes.project5);
 showDivs("project6", projectIndexes.project6);
-// showDivs("project7", projectIndexes.project7);
+showDivs("project7", projectIndexes.project7);
 
 function plusDivs(project, n) {
 	showDivs(project, projectIndexes[project] += n);
